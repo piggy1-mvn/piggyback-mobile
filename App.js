a66dc828bc040a5285b3808555e9b4f068c01d1b
unknown <elizabethjohnjk333@gmail.com>
2019-08-22 19:46:03 +0800

    react native hybrid mobile app

---
+++ App.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';	import { Text, View } from 'react-native';
+export default class HelloWorldApp extends Component {	  render() {
+	    return (
+			<View>
+			<Text>Hello world from piggyIncentive</Text>
+	               </View>	    );
+ }
+}
