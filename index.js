aa30f9f20e8b1c6f12582f3db4aeb9a0d7bb4428
Rajan Gupta <rajan0493@gmail.com>
2019-09-11 16:04:13 +0800

    Fixed app booting

--- index.js
+++ index.js
@@ -4,7 +4,6 @@
 
 import {AppRegistry} from 'react-native';
 import App from './App';
-import locationApp from './src/services/locationService.js'
 import Fblogin from './src/components/Fblogin.js'
 import {name as appName} from './app.json';
 
